46209e49d19ae18ce8557898863d1bc8b8682abe
Евгений Москалянов <emos@inbox.ru>
2023-07-29 21:43:52 +0300

    Рефакторинг базового модуля для тестирования

--- backend/users/tests/base.py
+++ backend/users/tests/base.py
@@ -5,25 +5,54 @@ from rest_framework.test import APITestCase
 
 TEST_HOST = 'http://testserver'
 
-class UserEndpointTestCase(APITestCase):
-    Model = get_user_model()
-    BASE_URL = '/api/users/'
 
+class EndpointTestCase(APITestCase):
+
+    def do_request_and_check_response(
+        self, client, method, url, request_data, exp_response_data, exp_status, **kwargs
+    ):
+        func = getattr(client, method)
+        # Act
+        self.response = func(url, request_data, format='json', **kwargs)
+        # Assert on response
+        with self.subTest():
+            self.assertEqual(self.response.status_code, exp_status)
+        response_data = self.response.json()
+        self.assertEqual(response_data, exp_response_data)
+        return response_data
+
+
+class EndpointModelMixin:
     @classmethod
     def get_instance_data(cls, instance, **kwargs):
         data = {
             field_name: getattr(instance, field_name)
-            for field_name in (
-                'id', 'email', 'username', 'first_name', 'last_name'
-            )
+            for field_name in cls.INSTANCE_FIELDS
         }
         data.update(**kwargs)
         return data
-        
+
+    @classmethod
+    def create_instance(cls, data, **kwargs):
+        data.update(**kwargs)
+        return cls.Model.objects.create(**data)
+
+    @classmethod
+    def create_instances(cls, data_seq):
+        for data in data_seq:
+            cls.create_instance(data)
+
+
+class UserEndpointTestCase(EndpointTestCase, EndpointModelMixin):
+    Model = get_user_model()
+    INSTANCE_FIELDS = (
+        'id', 'email', 'username', 'first_name', 'last_name'
+    )
+    BASE_URL = '/api/users/'
+
     @classmethod
     def create_data(cls, *, n='test', **kwargs):
         data = dict(
-            # id = id,
             email=f'mail_{n}@email.any',
             username=f'user_{n}',
             first_name=f'first_name_{n}',
@@ -35,16 +64,6 @@ class UserEndpointTestCase(APITestCase):
     @classmethod
     def get_data_iter(cls, iter):
         return (cls.create_data(n=i) for i in iter)
-
-    @classmethod
-    def create_instance(cls, data, **kwargs):
-        data.update(**kwargs)
-        return cls.Model.objects.create(**data)
-
-    @classmethod
-    def create_instances(cls, data_seq):
-        for data in data_seq:
-            cls.create_instance(data)
 
     def check_data_is_dict_with_proper_keys(self, data, keys):
         self.assertIsInstance(data, dict)
@@ -64,19 +83,6 @@ class UserEndpointTestCase(APITestCase):
         for key in kwargs:
             with self.subTest(key=key):
                 self.assertEqual(data[key], kwargs[key])
-
-    def do_request_and_check_response(
-        self, client, method, url, request_data, exp_response_data, exp_status, **kwargs
-    ):
-        func = getattr(client, method)
-        # Act
-        self.response = func(url, request_data, format='json', **kwargs)
-        # Assert on response
-        with self.subTest():
-            self.assertEqual(self.response.status_code, exp_status)
-        response_data = self.response.json()
-        self.assertEqual(response_data, exp_response_data)
-        return response_data
 
 
 def get_model_pk_set(model):
