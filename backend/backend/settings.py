2ecf4283d4f90eda7ab3b1b27c4bc6e7b99052ef
Евгений Москалянов <emos@inbox.ru>
2023-07-29 20:40:48 +0300

    Текущий пользователь протестирован и исправлен

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -141,6 +141,7 @@ REST_FRAMEWORK = {
 DJOSER = {
     'SERIALIZERS': {
         'user': 'users.serializers.UserSerializer',
+        'current_user': 'users.serializers.UserSerializer',
         'user_create': 'users.serializers.UserCreateSerializer',
     },
     'PERMISSIONS': {
