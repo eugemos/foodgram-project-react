3b38036ba504625780813333b06d34bc5bfe95f0
Евгений Москалянов <emos@inbox.ru>
2023-08-07 22:12:02 +0300

    Упрощены фикстуры для получения рецепта

--- backend/tests/api/recipes/test_recipe_detail.py
+++ backend/tests/api/recipes/test_recipe_detail.py
@@ -7,16 +7,16 @@ from .base import RecipeEndpointTestCase
 
 
 class RecipeDetailEndpointTestCase(RecipeEndpointTestCase):
-    TAGS_PER_RECIPE = 2
-    INGREDIENTS_PER_RECIPE = 2
-    FIXTURE_RECIPE_COUNT = 2**3
-    FIXTURE_TAG_COUNT = FIXTURE_RECIPE_COUNT + TAGS_PER_RECIPE
-    FIXTURE_INGREDIENT_COUNT = FIXTURE_RECIPE_COUNT + INGREDIENTS_PER_RECIPE
-    FIXTURE_AUTHOR_COUNT = 2
-    recipe_ids = range(1, FIXTURE_RECIPE_COUNT+1)
+    # TAGS_PER_RECIPE = 2
+    # INGREDIENTS_PER_RECIPE = 2
+    # FIXTURE_RECIPE_COUNT = 2**3
+    FIXTURE_TAG_COUNT = 3
+    FIXTURE_INGREDIENT_COUNT = 3
+    # FIXTURE_AUTHOR_COUNT = 2
+    # recipe_ids = range(1, FIXTURE_RECIPE_COUNT+1)
     tag_ids = range(1, FIXTURE_TAG_COUNT+1)
     ingredient_ids = range(1, FIXTURE_INGREDIENT_COUNT+1)
-    author_ids = range(1, FIXTURE_AUTHOR_COUNT+1)
+    # author_ids = range(1, FIXTURE_AUTHOR_COUNT+1)
 
     @classmethod
     def setUpClass(cls):
@@ -24,21 +24,15 @@ class RecipeDetailEndpointTestCase(RecipeEndpointTestCase):
         cls.tags = TestTag.create_instances(cls.tag_ids)
         cls.ingredients = TestIngredient.create_instances(cls.ingredient_ids)
         # cls.authors = tuple(TestUser.create_instance(n) for n in cls.author_ids)
-        cls.authors = TestUser.create_instances(cls.author_ids)
+        cls.author = TestUser.create_instance('author')
         cls.client_user = TestUser.create_instance(TestUser.create_data(n='client'))
-        recipes = cls.create_instances(
-            cls.create_data(
-                n=n,
-                author=cls.authors[n%2],
-                image=File(open('tests/data/test.png'), name=f'recipe_{n}.png'),
-            )
-            for n in cls.recipe_ids
+        cls.recipe = cls.create_instance(
+            author=cls.author,
+            image=File(open('tests/data/test.png'), name=f'recipe.png'),
         )
-        for recipe in recipes:
-            i = recipe.pk - 1
-            recipe.set_tags(cls.tags[i:i+cls.TAGS_PER_RECIPE])
-            for ingredient in cls.ingredients[i:i+cls.INGREDIENTS_PER_RECIPE]:
-                recipe.add_ingredient(ingredient, ingredient.pk)
+        cls.recipe.set_tags(cls.tags)
+        for ingredient in cls.ingredients:
+            cls.recipe.add_ingredient(ingredient, ingredient.pk)
 
     def test_fixture(self):
-        self.assertEqual(self.Model.objects.count(), self.FIXTURE_RECIPE_COUNT)  
+        self.assertEqual(self.Model.objects.count(), 1)  
