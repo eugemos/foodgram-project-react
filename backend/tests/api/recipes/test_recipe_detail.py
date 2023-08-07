8921d4def2dd3253f995f54cea84d5776005c349
Евгений Москалянов <emos@inbox.ru>
2023-08-08 00:06:42 +0300

    Продолжается работа над тестами для получения рецептов.

--- backend/tests/api/recipes/test_recipe_detail.py
+++ backend/tests/api/recipes/test_recipe_detail.py
@@ -1,6 +1,10 @@
-from django.core.files import File 
+from django.core.files import File
+from django.conf import settings
+from rest_framework import status
+from rest_framework.test import APIClient
 
 from tests.base import (
+    TEST_HOST,
     TestIngredient, TestTag, TestUser
 )
 from .base import RecipeEndpointTestCase
@@ -14,15 +18,15 @@ class RecipeDetailEndpointTestCase(RecipeEndpointTestCase):
     FIXTURE_INGREDIENT_COUNT = 3
     # FIXTURE_AUTHOR_COUNT = 2
     # recipe_ids = range(1, FIXTURE_RECIPE_COUNT+1)
-    tag_ids = range(1, FIXTURE_TAG_COUNT+1)
-    ingredient_ids = range(1, FIXTURE_INGREDIENT_COUNT+1)
+    tag_fids = range(1, FIXTURE_TAG_COUNT+1)
+    ingredient_fids = range(1, FIXTURE_INGREDIENT_COUNT+1)
     # author_ids = range(1, FIXTURE_AUTHOR_COUNT+1)
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.tags = TestTag.create_instances(cls.tag_ids)
-        cls.ingredients = TestIngredient.create_instances(cls.ingredient_ids)
+        cls.tags = TestTag.create_instances(cls.tag_fids)
+        cls.ingredients = TestIngredient.create_instances(cls.ingredient_fids)
         # cls.authors = tuple(TestUser.create_instance(n) for n in cls.author_ids)
         cls.author = TestUser.create_instance('author')
         cls.client_user = TestUser.create_instance(TestUser.create_data(n='client'))
@@ -34,5 +38,73 @@ class RecipeDetailEndpointTestCase(RecipeEndpointTestCase):
         for ingredient in cls.ingredients:
             cls.recipe.add_ingredient(ingredient, ingredient.pk)
 
-    def test_fixture(self):
-        self.assertEqual(self.Model.objects.count(), 1)  
+        cls.base_exp_response_data = cls.create_data(
+            id = cls.recipe.pk,
+            tags = [TestTag.create_data(n=fid, id=fid) for fid in cls.tag_fids],
+            author = TestUser.create_data(
+                n='author', id=cls.author.pk, is_subscribed=False
+            ),
+            ingredients = [
+                TestIngredient.create_data(n=fid, id=fid, amount=fid)
+                for fid in cls.ingredient_fids
+            ],
+            is_favorited=False,
+            is_in_shopping_cart=False,
+            image=f'{TEST_HOST}{settings.MEDIA_URL}{cls.recipe.image.name}'
+        )
+        assert set(cls.base_exp_response_data.keys()) == set(cls.OUTPUT_FIELDS)
+
+    def setUp(self):
+        super().setUp()
+        self.auth_client = APIClient()
+        self.auth_client.force_authenticate(user=self.client_user)
+        # self.maxDiff = None
+
+    def test_auth_user_can_retrieve_recipe_detail(self):
+        exp_response_data = self.base_exp_response_data
+        self.do_auth_request_and_check_response(
+            self.recipe.pk, exp_response_data, status.HTTP_200_OK
+        )
+
+    def test_auth_user_can_retrieve_recipe_detail_1(self):
+        self.client_user.subscribe_to(self.author)
+        
+        exp_response_data = dict(
+            self.base_exp_response_data,
+            author = TestUser.create_data(
+                n='author', id=self.author.pk, is_subscribed=True
+            ),
+        )
+        self.do_auth_request_and_check_response(
+            self.recipe.pk, exp_response_data, status.HTTP_200_OK
+        )
+
+    def test_anon_user_can_retrieve_recipe_detail(self):
+        exp_response_data = self.base_exp_response_data
+        self.do_anon_request_and_check_response(
+            self.recipe.pk, exp_response_data, status.HTTP_200_OK
+        )
+
+    def do_anon_request_and_check_response(
+        self, id, exp_response_data, exp_status
+    ):
+        return self.do_request_and_check_response(
+            self.client, id, exp_response_data, exp_status
+        )
+
+    def do_auth_request_and_check_response(
+        self, id, exp_response_data, exp_status
+    ):
+        return self.do_request_and_check_response(
+            self.auth_client, id, exp_response_data, exp_status
+        )
+
+    def do_request_and_check_response(
+        self, client, id, exp_response_data, exp_status
+    ):
+        return super().do_request_and_check_response(
+            client, 'get', f'{self.BASE_URL}{id}/', 
+            None, exp_response_data, exp_status
+        )
+
+
