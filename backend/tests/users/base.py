3cb882b2e769ce5773f0c70511f18eaa1253dd54
emos <emos@ntk2.local>
2023-08-11 10:49:36 +0300

    Выполнен рафакторинг тестов для списка пользователей.

--- backend/tests/users/base.py
+++ backend/tests/users/base.py
@@ -8,25 +8,6 @@ class UserEndpointTestCase(EndpointTestCase, TestUser):
     def get_data_iter(cls, iter):
         return (cls.create_data(fid=i) for i in iter)
 
-    def check_data_is_dict_with_proper_keys(self, data, keys):
-        self.assertIsInstance(data, dict)
-        self.assertEqual(set(data.keys()), set(keys))
-
-    def check_data_is_list_of_proper_length(self, data, length):
-        self.assertIsInstance(data, list)
-        self.assertEqual(len(data), length)
-
-    def check_data_is_dict_with_proper_items(self, data, instance=None, **kwargs):
-        if instance is not None:
-            for key in data:
-                with self.subTest(key=key):
-                    if hasattr(instance, key):
-                        self.assertEqual(data[key], getattr(instance, key))
-        
-        for key in kwargs:
-            with self.subTest(key=key):
-                self.assertEqual(data[key], kwargs[key])
-
 
 class AuthEndpointTestCase(EndpointTestCase, TestUser):
     BASE_URL = '/api/auth/token/'
--- backend/tests/users/test_user_list.py
+++ backend/tests/users/test_user_list.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import AnonymousUser
 from django.conf import settings
 from rest_framework import status
+from rest_framework.test import APIClient
 
-from tests.base import TEST_HOST
+from tests.base import TEST_HOST, nrange
 from .base import UserEndpointTestCase
 
 
@@ -10,64 +11,18 @@ DEFAULT_PAGE_SIZE = settings.REST_FRAMEWORK['PAGE_SIZE']
 
 
 class UserListTestCase(UserEndpointTestCase):
-    OUTPUT_FIELDS = (
-        'id',
-        'email',
-        'username',
-        'first_name',
-        'last_name',
-        'is_subscribed', 
-    )
 
-    OUTPUT_PAGINATOR_FIELDS = (
-        'count',
-        'next',
-        'previous',
-        'results',
-    )
-
-    DEFAULT_SUBSCRIPTIONS = {1: (2,)}
-
-    def test_list_action_without_params(self):
-        page_size = DEFAULT_PAGE_SIZE
-        self.prepare(instance_count=page_size, page_size=page_size)
-        self.perform_test(1)
-
-    def test_list_action_without_params_authenticated(self):
-        page_size = DEFAULT_PAGE_SIZE
-        self.prepare(
-            instance_count=page_size, 
-            page_size=page_size, 
-            client_user_id=1, 
-            subscriptions={1: (2,4,6)}
-        )
-        self.perform_test(1)
-
-    def test_list_action_without_params_1(self):
-        page_size = DEFAULT_PAGE_SIZE
-        self.prepare(instance_count=page_size+1, page_size=page_size)
-        self.perform_test(1)
-
-    def test_list_action_without_params_2(self):
-        page_size = DEFAULT_PAGE_SIZE
-        self.prepare(instance_count=page_size-1, page_size=page_size)
-        self.perform_test(1)
-
-    def test_list_action_with_params(self):
-        self.prepare(instance_count=16, page_size=5)
-        for page in range(1, 5):
-            with self.subTest(page=page):
-                self.perform_test(page, dict(limit=self.page_size, page=page))
+    DEFAULT_SUBSCRIPTIONS = {1: (2, DEFAULT_PAGE_SIZE-1)}
 
     def prepare(self, *, 
                 instance_count, 
                 page_size, 
-                client_user_id=None, 
-                subscriptions=DEFAULT_SUBSCRIPTIONS):
-        self.client_user = AnonymousUser()
+                subscriptions=DEFAULT_SUBSCRIPTIONS
+    ):
+        self.maxDiff = None
         self.instance_count = instance_count
         self.page_size = page_size
-        self.create_instances(self.get_data_iter(range(self.instance_count)))
+        self.create_instances(self.get_data_iter(nrange(1, self.instance_count)))
 
         assert self.Model.objects.count() == instance_count
         self.underfull_page_size = instance_count % page_size
@@ -75,52 +30,67 @@ class UserListTestCase(UserEndpointTestCase):
             instance_count // page_size 
             + (self.underfull_page_size > 0)
         )
-        if client_user_id:
-            self.client_user = self.Model.objects.get(pk=client_user_id)
-            self.client.force_authenticate(user=self.client_user)
-           
+
         for user in (self.Model.objects.get(pk=pk) for pk in subscriptions):
             user.set_subscriptions(
                 self.Model.objects.get(pk=pk) for pk in subscriptions[user.pk]
             )
-            
-    def perform_test(self, page, params=dict()):
-        # Act
-        self.response = self.client.get(
-            self.BASE_URL, params, format='json', follow=True
+
+    def test_list_action_without_params(self):
+        page_size = DEFAULT_PAGE_SIZE
+        subscriptions = self.DEFAULT_SUBSCRIPTIONS
+        self.prepare(instance_count=page_size, page_size=page_size, subscriptions=subscriptions)
+        for client_id in (0, 1, 2):
+            with self.subTest(client_id=client_id):
+                self.perform_test(
+                    self.create_client(client_id),
+                    1,
+                    subscriptions.get(client_id, ())
+                )
+
+    def test_list_action_without_params_1(self):
+        page_size = DEFAULT_PAGE_SIZE
+        self.prepare(instance_count=page_size+1, page_size=page_size)
+        self.perform_test(self.client, 1)
+
+    def test_list_action_without_params_2(self):
+        page_size = DEFAULT_PAGE_SIZE
+        self.prepare(instance_count=page_size-1, page_size=page_size)
+        self.perform_test(self.client, 1)
+
+    def test_list_action_with_params(self):
+        self.prepare(instance_count=16, page_size=5)
+        for page in range(1, 5):
+            with self.subTest(page=page):
+                self.perform_test(self.client, page, params=dict(limit=self.page_size, page=page))
+
+    def perform_test(self, client, page, subscribed=(), *, params=dict()):
+        self.do_request_and_check_response(
+            client,
+            params,
+            self.create_exp_response_data(page, params, subscribed)
         )
-        # Assert
-        self.assertEqual(self.response.status_code, status.HTTP_200_OK)
-        self.check_paginator_output(
+           
+    def create_exp_response_data(self, page, params, subscribed):
+        page_size = self.page_size if page != self.page_count else self.last_page_size()
+        start_fid = (page - 1)*self.page_size + 1
+        return dict(
             count=self.instance_count,
             previous=self.previous_page_link(page, params),
             next=self.next_page_link(page, params),
-        )       
-        self.check_object_list(
-            self.page_size if page != self.page_count else self.last_page_size(),
-            (page - 1)*self.page_size + 1
+            results= [
+                self.create_data(fid=fid, id=fid, is_subscribed=(fid in subscribed))
+                for fid in nrange(start_fid, page_size)
+            ]          
         )
 
-    def check_paginator_output(self, **kwargs):
-        data = self.response.json()
-        self.check_data_is_dict_with_proper_keys(data, self.OUTPUT_PAGINATOR_FIELDS)
-        self.check_data_is_dict_with_proper_items(data, **kwargs)
+    def create_client(self, user_id):
+        client = APIClient()
+        if user_id > 0:
+            client_user = self.Model.objects.get(pk=user_id)
+            client.force_authenticate(user=client_user)
 
-    def check_object_list(self, page_size, start_id):
-        data = self.response.json()['results']
-        self.check_data_is_list_of_proper_length(data, page_size)
-        for i in range(page_size):
-            item = data[i]
-            pk = item['id']
-            instance = self.Model.objects.get(pk=pk)
-            with self.subTest(what=f'Проверка {i}-го элемента страницы (pk={pk})'):               
-                self.check_data_is_dict_with_proper_keys(item, self.OUTPUT_FIELDS)
-                self.assertEqual(pk, i+start_id)
-                self.check_data_is_dict_with_proper_items(
-                    item,
-                    instance,
-                    is_subscribed=self.is_client_subscribed(instance)
-                )
+        return client
 
     def previous_page_link(self, page, request_params):
         if page <= 1:
@@ -159,7 +129,13 @@ class UserListTestCase(UserEndpointTestCase):
             else self.underfull_page_size
         )
     
-    def is_client_subscribed(self, user):
-        return (self.client_user.is_authenticated 
-                and self.client_user.is_subscribed_to(user))
+    def do_request_and_check_response(
+        self, client, request_data, exp_response_data,
+        exp_status=status.HTTP_200_OK
+    ):
+        return super().do_request_and_check_response(
+            client, 'get', self.BASE_URL, 
+            request_data, exp_response_data, exp_status,
+            follow=True
+        )
     
