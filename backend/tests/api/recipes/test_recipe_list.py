f9c982c7bc725b1d6f0633fd98da0d7196725daf
emos <emos@ntk2.local>
2023-08-11 12:45:12 +0300

    Начал работать над тестами для списка рецептов.

---
+++ backend/tests/api/recipes/test_recipe_list.py
@@ -0,0 +1,9 @@
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from tests.base import TEST_HOST, DEFAULT_PAGE_SIZE, nrange
+from .base import UserEndpointTestCase
+
+
+class RecipeListTestCase(RecipeEndpointTestCase):
+    pass
