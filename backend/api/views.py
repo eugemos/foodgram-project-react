a33901cc94ac47e88ae746b0898e8a27c8135419
Евгений Москалянов <emos@inbox.ru>
2023-08-13 10:09:23 +0300

    В списке рецептов реализована фильтрация.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,5 @@
+import re
+
 import django_filters.rest_framework as dj_filters
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 from rest_framework.permissions import IsAuthenticated
@@ -34,8 +36,34 @@ class RecipeViewSet(ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     permission_classes = (RecipesPermission,)
-    # filter_backends = (dj_filters.DjangoFilterBackend,)
-    # filterset_class = IngredientFilterSet
+
+    def get_queryset(self):
+        user = self.request.user
+        qs = super().get_queryset()
+        if self.request.query_params.get('is_favorited', 0) == '1':
+            if user.is_authenticated:
+                qs = qs.filter(in_favore=user)
+            else:
+                qs = qs.none()
+
+        if self.request.query_params.get('is_in_shopping_cart', 0) == '1':
+            if user.is_authenticated:
+                qs = qs.filter(in_shopping_cart=user)
+            else:
+                qs = qs.none()
+
+        if 'author' in self.request.query_params:
+            author_id = self.request.query_params['author']
+            if re.fullmatch('\d+', author_id):
+                qs = qs.filter(author__id=author_id)
+            else:
+                qs = qs.none()
+
+        for tag in self.request.query_params.getlist('tags'):
+            qs = qs.filter(tags__slug=tag)
+
+        return qs
+
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
