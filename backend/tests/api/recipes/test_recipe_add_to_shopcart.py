e3f34432382d7d9e406d6ab287978ce056833504
Евгений Москалянов <emos@inbox.ru>
2023-08-13 14:40:28 +0300

    Рефакторинг тестов для добавления рецепта в список покупок.

--- backend/tests/api/recipes/test_recipe_add_to_shopcart.py
+++ backend/tests/api/recipes/test_recipe_add_to_shopcart.py
@@ -19,15 +19,15 @@ class RecipeAddToShopcartTestCase(RecipeEndpointTestCase):
         cls.recipe = cls.create_recipe(
             'test', cls.author, cls.tags, cls.ingredients
         )
-        cls.create_recipe(
+        cls.recipe_other = cls.create_recipe(
             'other', cls.user, cls.tags, cls.ingredients
         )
+        cls.user.add_to_shopping_cart(cls.recipe_other)
 
     def setUp(self):
         super().setUp()
         self.user_client = APIClient()
         self.user_client.force_authenticate(user=self.user)
-        self.assertEqual(self.user.shopping_cart.count(), 0)
 
     def test_auth_user_can_add_recipe_to_shopcart(self):
         exp_response_data = self.create_exp_response_data(self.recipe, fid='test')
@@ -35,33 +35,29 @@ class RecipeAddToShopcartTestCase(RecipeEndpointTestCase):
             self.user_client, self.recipe.pk, exp_response_data,
             status.HTTP_201_CREATED
         )
-        self.assertEqual(self.user.shopping_cart.count(), 1)
+        self.assertEqual(self.user.shopping_cart.count(), 2)
         self.assertTrue(self.user.has_in_shopping_cart(self.recipe))
 
     def test_anon_user_cant_add_recipe_to_shopcart(self):
         exp_response_data = self.UNAUTHORIZED_ERROR_RESPONSE_DATA
-        self.do_request_and_check_response(
+        self.check_request_fails(
             self.client, self.recipe.pk, exp_response_data,
             status.HTTP_401_UNAUTHORIZED
         )
-        self.assertEqual(self.user.shopping_cart.count(), 0)
 
     def test_auth_user_cant_add_recipe_to_shopcart_twice(self):
-        self.user.add_to_shopping_cart(self.recipe)
         exp_response_data = dict(errors='Этот рецепт уже есть в этом списке.')
-        self.do_request_and_check_response(
-            self.user_client, self.recipe.pk, exp_response_data,
+        self.check_request_fails(
+            self.user_client, self.recipe_other.pk, exp_response_data,
             status.HTTP_400_BAD_REQUEST
         )
-        self.assertEqual(self.user.shopping_cart.count(), 1)
 
     def test_request_to_unexistent_recipe_fails(self):
         exp_response_data = self.PAGE_NOT_FOUND_RESPONSE_DATA
-        self.do_request_and_check_response(
+        self.check_request_fails(
             self.user_client, 10, exp_response_data,
             status.HTTP_404_NOT_FOUND
         )
-        self.assertEqual(self.user.shopping_cart.count(), 0)
 
     @classmethod
     def create_exp_response_data(
@@ -77,6 +73,15 @@ class RecipeAddToShopcartTestCase(RecipeEndpointTestCase):
         # cls.assertEqual(set(data.keys()), set(cls.OUTPUT_FIELDS))
         assert set(data.keys()) == set(cls.OUTPUT_FIELDS)
         return data
+
+    def check_request_fails(
+        self, client, id, exp_response_data, exp_status
+    ):
+        self.do_request_and_check_response(
+            client, id, exp_response_data, exp_status
+        )
+        self.assertEqual(self.user.shopping_cart.count(), 1)
+        self.assertTrue(self.user.has_in_shopping_cart(self.recipe_other))
 
     def do_request_and_check_response(
         self, client, id, exp_response_data, exp_status
