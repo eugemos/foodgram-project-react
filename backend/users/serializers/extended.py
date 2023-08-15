b3357e95b727b5304c757859e7e0d2c6672310a9
emos <emos@ntk2.local>
2023-08-15 14:59:39 +0300

    Реализована подписка на автора. Устранён возникший циркулярный импорт.

---
+++ backend/users/serializers/extended.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from api.serializers import ReducedRecipeSerializer
+from .base import UserSerializer
+
+class ExtendedUserSerializer(UserSerializer):
+    recipes = ReducedRecipeSerializer(read_only=True)
+    recipes_count = serializers.SerializerMethodField()
+
+    class Meta(UserSerializer.Meta):
+        fields = UserSerializer.Meta.fields + ('recipes', 'recipes_count')
+
+    def get_recipes_count(self, user):
+        return user.recipes.count()
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,11 @@
+from django.shortcuts import get_object_or_404
 from djoser.views import TokenCreateView, UserViewSet as DjoserUserViewSet
 from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+
+from .models import User
+from .serializers.extended import ExtendedUserSerializer
 
 
 class GetTokenView(TokenCreateView):
@@ -12,4 +18,26 @@ class GetTokenView(TokenCreateView):
 
 
 class UserViewSet(DjoserUserViewSet):
-    pass
+
+    @action(['post'], detail=True, serializer_class=ExtendedUserSerializer,
+            permission_classes=[IsAuthenticated])
+    def subscribe(self, request, pk):
+        author = get_object_or_404(User, pk=pk)
+        if request.user == author:
+            return Response(
+                dict(errors='Нельзя подписаться на самого себя.'),
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if request.user.is_subscribed_to(author):
+            return Response(
+                dict(errors='Вы уже подписаны на этого автора.'),
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        request.user.subscribe_to(author)
+        serializer = self.get_serializer(author)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
