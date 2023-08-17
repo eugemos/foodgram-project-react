8d528f5353b03e7405ae68332c450ccf8c1b3f1a
emos <emos@ntk2.local>
2023-08-17 16:40:29 +0300

    Реализована команда для загрузки ингредиентов из файла в формате json.

---
+++ backend/api/management/commands/load_ingredients.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand, CommandError
+from rest_framework.parsers import JSONParser
+
+from api.serializers import IngredientSerializer
+
+
+class Command(BaseCommand):
+    help = "Загружает ингредиенты в БД из файла в формате JSON."
+
+    def add_arguments(self, parser):
+        parser.add_argument('file_name', metavar='<file name>', help='Имя файла с ингредиентами.')
+
+    def handle(self, *args, **options):
+        file_name = options['file_name']
+        try:
+            with open(file_name, 'rb') as file:
+                data = JSONParser().parse(file)
+        except Exception as e:
+            return f'Ошибка: {e}'
+
+        serializer = IngredientSerializer(data=data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+            return 'Загрузка выполнена успешно.'
+        
+        return f'Файл содержит некорректные данные:\n{serializer.errors}'
