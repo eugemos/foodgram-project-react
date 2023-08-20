2e1a047764483e8cf2393078ec153a9209321119
Евгений Москалянов <emos@inbox.ru>
2023-08-20 14:37:33 +0300

    Удаление папки tests из проекта бэкэнда.

--- backend/tests/users/base.py
+++
@@ -1,9 +0,0 @@
-from tests.base import EndpointTestCase, TestUser
-
-
-class UserEndpointTestCase(EndpointTestCase, TestUser):
-    BASE_URL = '/api/users/'
-
-
-class AuthEndpointTestCase(EndpointTestCase, TestUser):
-    BASE_URL = '/api/auth/token/'
