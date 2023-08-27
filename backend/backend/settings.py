42618cb5cf1b5680df965d422e8d6ac53e71d4cd
Евгений Москалянов <emos@inbox.ru>
2023-08-27 23:25:56 +0300

    Изменено местоположение БД

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -59,7 +59,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'data' / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
