c0183285c3819776f10e6201da7482c1fe5cd633
emos <emos@ntk2.local>
2023-09-04 11:35:54 +0300

    Модель IngredientOccurence внедрена на страницу добавления/изменения рецепта.

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,8 +1,12 @@
 """Содержит настройки административной панели для приложения recipes."""
+from django import forms
 from django.contrib import admin
 
 from .models import Tag, Ingredient, Recipe, IngredientOccurence
 
+
+class RecipeAdminForm(forms.ModelForm):
+    pass
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -17,12 +21,29 @@ class IngredientAdmin(admin.ModelAdmin):
     list_filter = ('name',)
 
 
+class IngredientOccurenceInline(admin.TabularInline):
+    """Настройки отображения модели IngredientOccurence в административной
+    панели.
+    """
+    model = IngredientOccurence
+    verbose_name = 'Ингредиент в этом рецепте'
+    verbose_name_plural = 'Ингредиенты в этом рецепте'
+    readonly_fields = ('measurement_unit',)
+    fields = ('ingredient', 'amount', 'measurement_unit')
+
+    @admin.display(description='Ед. изм.')
+    def measurement_unit(self, occurence):
+        return occurence.ingredient.measurement_unit
+
+
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     """Настройки отображения модели Recipe в административной панели."""
     readonly_fields = ('in_favor_count',)
     list_display = ('name', 'author_name')
     list_filter = ('author', 'name', 'tags')
+    inlines = (IngredientOccurenceInline,)
+    form = RecipeAdminForm
 
     @admin.display(description='Автор')
     def author_name(self, recipe):
@@ -33,12 +54,3 @@ class RecipeAdmin(admin.ModelAdmin):
     def in_favor_count(self, recipe):
         """Возвращает количество добавлений рецепта в избранное."""
         return recipe.in_favore.count()
-
-
-@admin.register(IngredientOccurence)
-class IngredientOccurenceAdmin(admin.ModelAdmin):
-    """Настройки отображения модели IngredientOccurence в административной
-    панели.
-    """
-    list_display = ('recipe', 'ingredient', 'amount')
-    list_filter = ('recipe', 'ingredient')
