a844fe11fed99b65f84ad660ac09d252e8db1612
emos <emos@ntk2.local>
2023-09-04 16:53:14 +0300

    Исправления в админке по замечаниям ревью.

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,12 +1,48 @@
 """Содержит настройки административной панели для приложения recipes."""
 from django import forms
 from django.contrib import admin
+from django.core.exceptions import ValidationError
 
 from .models import Tag, Ingredient, Recipe, IngredientOccurence
 
 
-class RecipeAdminForm(forms.ModelForm):
-    pass
+class IngredientOccurenceAdminFormSet(forms.BaseInlineFormSet):
+    # validate_min = True
+    def clean(self):
+        if any(self.errors):
+            return
+
+        if all(self._should_delete_form(form) for form in self.forms):
+            raise ValidationError('Нельзя удалить все ингредиенты.')
+
+
+class IngredientOccurenceInline(admin.TabularInline):
+    """Настройки отображения модели IngredientOccurence в административной
+    панели.
+    """
+    # formset = IngredientOccurenceAdminFormSet
+    model = IngredientOccurence
+    # validate_min = True
+    extra = 0
+    min_num = 1
+    verbose_name = 'Ингредиент в этом рецепте'
+    verbose_name_plural = 'Ингредиенты в этом рецепте'
+    readonly_fields = ('measurement_unit',)
+    fields = ('ingredient', 'amount', 'measurement_unit')
+
+    @admin.display(description='Ед. изм.')
+    def measurement_unit(self, occurence):
+        return occurence.ingredient.measurement_unit
+
+    def get_formset(self, request, obj=None, **kwargs):
+        kwargs.update(
+            validate_min=True,
+            error_messages=dict(
+                too_few_forms='Рецепт должен содержать хотя бы один ингредиент.'
+            )
+        )
+        return super().get_formset(request, obj, **kwargs)
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -21,21 +57,6 @@ class IngredientAdmin(admin.ModelAdmin):
     list_filter = ('name',)
 
 
-class IngredientOccurenceInline(admin.TabularInline):
-    """Настройки отображения модели IngredientOccurence в административной
-    панели.
-    """
-    model = IngredientOccurence
-    verbose_name = 'Ингредиент в этом рецепте'
-    verbose_name_plural = 'Ингредиенты в этом рецепте'
-    readonly_fields = ('measurement_unit',)
-    fields = ('ingredient', 'amount', 'measurement_unit')
-
-    @admin.display(description='Ед. изм.')
-    def measurement_unit(self, occurence):
-        return occurence.ingredient.measurement_unit
-
-
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     """Настройки отображения модели Recipe в административной панели."""
@@ -43,7 +64,6 @@ class RecipeAdmin(admin.ModelAdmin):
     list_display = ('name', 'author_name')
     list_filter = ('author', 'name', 'tags')
     inlines = (IngredientOccurenceInline,)
-    form = RecipeAdminForm
 
     @admin.display(description='Автор')
     def author_name(self, recipe):
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,11 +1,25 @@
 """Содержит настройки административной панели для приложения users."""
+from django import forms
 from django.contrib import admin
+from django.core.exceptions import ValidationError
 
 from .models import User
+
+
+class UserAdminForm(forms.ModelForm):
+    def clean_subscribed_to(self):
+        # print(f'\nINSTANCE: {self.instance.pk}\n')
+        # print(f'\nC_DATA: {self.cleaned_data}\n')
+        value = self.cleaned_data['subscribed_to']
+        if value.filter(pk=self.instance.pk).exists():
+            raise ValidationError('Нельзя подписаться на самого себя.')
+            
+        return value
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     """Настройки отображения модели users в административной панели."""
+    form = UserAdminForm
     list_display = ('username', 'email', 'last_name', 'first_name')
     list_filter = ('username', 'email', 'is_staff', )
